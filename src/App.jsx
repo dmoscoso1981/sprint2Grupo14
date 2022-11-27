014e52cf4d219a4447e4f144a85af2d9012458a6
dmoscoso1981 <degonzalezm1@gmail.com>
2022-11-27 00:54:25 -0500

    primer intento sprint 2

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Instaya</h1>
+      
+      <span>Empresa de entrega de paquetes</span>
+      
+      <ul className="card">
+        <li>Registro</li>
+        <li>Iniciar sesion</li>
+        <li>Rastreo de paquetes</li>
+        <li>Contactenos</li>
+      </ul>
+    </div>
+  );
+}
